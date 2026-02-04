f23a7e46cdef82f304f4545e92efacb7f59dae08
Gabriel Alcantara da Costa <gabrielallcantaracosta@gmail.com>
2026-02-04 19:17:43 -0300

    create real bublle sort

--- bubllesort/main.go
+++ bubllesort/main.go
@@ -2,94 +2,36 @@ package main
 
 import "fmt"
 
-// func buscarMenor(arr []int) int {
-// 	menor := arr[0]
-// 	menore_indice := 0
-
-// 	for i := 1; i < len(arr); i++ {
-// 		if arr[i] < menor {
-// 			menor = arr[i]
-// 			menore_indice = i
-// 		}
-// 	}
-
-// 	return menore_indice
-// }
-
-// func main() {
-
-// 	arr := []int{10, 23, 1, 215, 1612, 23, 15, 612, 123, 12}
-// 	novoArr := []int{}
-
-// 	for len(arr) > 0 {
-// 		menor := buscarMenor(arr)
-// 		novoArr = append(novoArr, arr[menor])
-
-// 		arr = append(arr[:menor], arr[menor+1:]...)
-// 	}
-
-// 	fmt.Println(novoArr)
-// }
-
-/*------RECURSIVA-------*/
-
-func minIndexRec(arr []int, i int, minIdx int) int {
-	if i >= len(arr) {
-		return minIdx
-	}
-
-	if arr[i] < arr[minIdx] {
-		minIdx = i
-	}
-
-	return minIndexRec(arr, i+1, minIdx)
-}
-
-func orderArrByMinToMax(arr *[]int, newArr *[]int) []int {
-	if len(*arr) != 0 {
-		minIdx := minIndexRec(*arr, 1, 0)
-
-		*newArr = append(*newArr, (*arr)[minIdx])
-
-		*arr = append((*arr)[:minIdx], (*arr)[minIdx+1:]...)
-
-		orderArrByMinToMax(arr, newArr)
-	}
-
-	return *newArr
-}
-
 func main() {
-	arr := []int{10, 23, 1, 215, 1612, 23, 15, 612, 123, 12}
-	novoArr := []int{}
+	arr := []int{123, 32, 1, 35, 2, 432, 321, 21}
 
-	arr = orderArrByMinToMax(&arr, &novoArr)
-	fmt.Scanf()
-	fmt.Println("novoArr:", novoArr)
-	fmt.Println("arr (restou):", arr)
+	//NAVEGAR POR TODO O ARRAY
+	for i := 0; i < len(arr)-1; i++ {
+		//DEFINE COMO TERMINADO
+		swapp := false
+		//NAVEGA NOVAMENTE PELO ARRAY ATE O ITEM QUE TEM O MAIOR VALOR
+		for j := 0; j < len(arr)-1-i; j++ {
+			//VERIFICANDO SE O ITEM ATUAL É MAIOR QUE O PROXIMO
+			if arr[j] > arr[j+1] {
+				//SE FOR MAIOR, FAZ A TROCA
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				//DEFINE QUE AINDA TEM TROCA A SE FAZER
+				swapp = true
+			}
+		}
+
+		/*
+			APOS IR VERIFICANDO SE O ITEM ATUAL É O MAIO, AO CHEGAR NO FINAL, VC VAI TER DEFINIDO QUE O ULTIMO ITEM
+			É O MAIOR IR TEM DO ARRAY, POR ISSO QUE AO INICIAR UMA NOVA NAVEGACAO PELO ARRY SECUNDARIO, FAZEMOS:
+			len(arr)-1 [PARA NAO ESTORA] -i [PARA DESCARTAR OS ITEMS QUE JA FORAM DEFINIDO COMO MAIOR]
+		*/
+
+		if !swapp {
+			break
+		}
+		fmt.Println(i)
+	}
+	fmt.Println(len(arr))
+
+	fmt.Println(arr)
 }
-
-/*------------------------*/
-
-// var maior int
-
-// func main() {
-// 	list := []int{10, 23, 1, 215, 1612, 23, 15, 612, 123, 12}
-// 	newList := []int{}
-
-// 	for len(list) > 0 {
-// 		maiorInx := 0
-
-// 		for i := 1; i < len(list); i++ {
-// 			if list[i] > list[maiorInx] {
-// 				maiorInx = i
-// 			}
-// 		}
-
-// 		newList = append(newList, list[maiorInx])
-
-// 		list = append(list[:maiorInx], list[maiorInx+1:]...)
-// 	}
-
-// 	fmt.Println(newList)
-// }
